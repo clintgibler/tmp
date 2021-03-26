866b319176a4afcc66e044352d6c40772558a960
Clint Gibler <clintgibler@users.noreply.github.com>
2021-03-26 09:51:47 -0700

    Update cookieDemoTest.php
    Co-authored-by: semgrep-dev[bot] <63493438+semgrep-dev[bot]@users.noreply.github.com>

--- cookieDemoTest.php
+++ cookieDemoTest.php
@@ -2,7 +2,8 @@
 
 // ruleid: checklist-cookie-fetch
 # Do not use $_COOKIE to get cookie values
-echo 'Hello ' . htmlspecialchars($_COOKIE["name"]) . '!';
+echo 'Hello ' . htmlspecialchars($wgRequest->getCookie("name")
+) . '!';
 $email = $_COOKIE["email"];
 
 // ok: checklist-cookie-fetch
